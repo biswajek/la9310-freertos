c93d5bef8e0603b44e925c44637cbb9eea13ae2a
Biswajeet <kumar.biswajeet@gmail.com>
2026-04-18 19:37:46 +0400

    Remove build artifacts, add .gitignore for tests/qemu/build/

--- Demo/CORTEX_M4_NXP_LA9310_GCC/tests/qemu/build/CMakeFiles/ipc_test.dir/home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/ms_procx_comm.c.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/ipc_test.dir/home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/ms_procx_comm.c.obj: \
- /home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/ms_procx_comm.c \
- /home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/tests/qemu/../../include/ms_logger.h \
- /home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/tests/qemu/../../include/ms_procx_comm.h
-/home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/tests/qemu/../../include/ms_logger.h:
-/home/biswajex/DivyaDrishti/external/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/tests/qemu/../../include/ms_procx_comm.h:
