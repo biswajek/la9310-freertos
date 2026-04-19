41154268f053bb69c94023bee12a975155cba6ea
Biswajeet <kumar.biswajeet@gmail.com>
2026-04-19 19:24:35 +0400

    build_release.sh: accept optional extra cmake flags after boot_mode
    Extra -D flags passed after the boot_mode argument are forwarded
    directly to cmake. Usage:
    
      ./build_release.sh -boot_mode=pci -DTEST_L1C_TASKS=ON

--- Demo/CORTEX_M4_NXP_LA9310_GCC/build_release.sh
+++ Demo/CORTEX_M4_NXP_LA9310_GCC/build_release.sh
@@ -3,29 +3,38 @@
 #Copyright 2022 NXP
 
 boot_mode=pcie
+extra_cmake_flags=""
 
-if [ "$#" != "1" ]; then
+if [ "$#" -lt "1" ]; then
 	echo "Invoke command as show below (boot_mode i2c,pcie)"
 	echo "./build_release.sh -boot_mode=i2c"
 	echo  "          OR                   "
-        echo "./build_release.sh -boot_mode=pci"
-else
-	file="include/la9310_boot_mode.h"
-	unlink $file
+	echo "./build_release.sh -boot_mode=pci"
+	echo  "          OR (with optional extra cmake flags)   "
+	echo "./build_release.sh -boot_mode=pci -DTEST_L1C_TASKS=ON"
+	exit 1
+fi
 
-	i2c_boot=`echo "$*" | grep -i "boot_mode=I2c"`
-	[ -n "$i2c_boot" ] && {
-		boot_mode=i2c
-	}
-	if [ "$boot_mode" = "pcie" ];then
-		echo "Build NLM for PCIe Host Mode.."
-		cmake -DCMAKE_TOOLCHAIN_FILE="./armgcc.cmake" -G "Unix Makefiles" -DTURN_ON_STANDALONE_MODE=OFF  -DCMAKE_BUILD_TYPE=Release  .
-		echo "#define TURN_ON_HOST_MODE  1" > $file
-	else
-		echo "Build NLM for I2C Standalone Mode.."
-		cmake -DCMAKE_TOOLCHAIN_FILE="./armgcc.cmake" -G "Unix Makefiles" -DTURN_ON_STANDALONE_MODE=ON  -DCMAKE_BUILD_TYPE=Release  .
-		echo "#define TURN_ON_STANDALONE_MODE  1" > $file
-	fi
+file="include/la9310_boot_mode.h"
+unlink $file 2>/dev/null || true
+
+i2c_boot=`echo "$1" | grep -i "boot_mode=I2c"`
+[ -n "$i2c_boot" ] && {
+	boot_mode=i2c
+}
+
+# Collect any extra cmake -D flags passed after the boot_mode argument
+shift
+extra_cmake_flags="$*"
+
+if [ "$boot_mode" = "pcie" ];then
+	echo "Build NLM for PCIe Host Mode.."
+	cmake -DCMAKE_TOOLCHAIN_FILE="./armgcc.cmake" -G "Unix Makefiles" -DTURN_ON_STANDALONE_MODE=OFF  -DCMAKE_BUILD_TYPE=Release  $extra_cmake_flags .
+	echo "#define TURN_ON_HOST_MODE  1" > $file
+else
+	echo "Build NLM for I2C Standalone Mode.."
+	cmake -DCMAKE_TOOLCHAIN_FILE="./armgcc.cmake" -G "Unix Makefiles" -DTURN_ON_STANDALONE_MODE=ON  -DCMAKE_BUILD_TYPE=Release  $extra_cmake_flags .
+	echo "#define TURN_ON_STANDALONE_MODE  1" > $file
 fi
 make -j4
 unlink $file
